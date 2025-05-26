61900d24c760f6e071d8bf33adf22e692bc85dc2
lab_services_student <maddison.botha@icloud.com>
2025-05-26 19:03:47 +0200

    Update to Score screen + Code Attribution

--- app/src/main/java/vcmsa/ci/imad2/MainActivity3.kt
+++ app/src/main/java/vcmsa/ci/imad2/MainActivity3.kt
@@ -53,3 +53,65 @@ class MainActivity3 : AppCompatActivity() {
         }
     }
 }
+
+// Title: How to achieve setOnClickListener in Kotlin
+// Author: Stack Overflow Community
+// Date: June 1, 2017
+// Version: 1
+// Available: https://stackoverflow.com/questions/44301301/android-how-to-achieve-setonclicklistener-in-kotlin
+
+// Title: Kotlin Conditions and If..Else
+// Author: W3Schools
+// Date: [Accessed May 26, 2025]
+// Version: 1
+// Available: https://www.w3schools.com/kotlin/kotlin_conditions.php
+
+// Title: Kotlin Android start new Activity
+// Author: Stack Overflow Community
+// Date: August 7, 2017
+// Version: 1
+// Available: https://stackoverflow.com/questions/45518139/kotlin-android-start-new-activity
+
+// Title: Arrays | Kotlin Documentation
+// Author: Kotlin Documentation
+// Date: [Accessed May 26, 2025]
+// Version: 1
+// Available: https://kotlinlang.org/docs/arrays.html
+
+// Title: Kotlin for Loop (With Examples)
+// Author: Programiz
+// Date: [Accessed May 26, 2025]
+// Version: 1
+// Available: https://www.programiz.com/kotlin-programming/for-loop
+
+// Title: How to get and set a text to TextView in Android using Kotlin?
+// Author: Stack Overflow Community
+// Date: May 21, 2017
+// Version: 1
+// Available: https://stackoverflow.com/questions/44096838/kotlin-how-to-get-and-set-a-text-to-textview-in-android-using-kotlin
+
+// Title: How to call finishAffinity() in Service class?
+// Author: Stack Overflow Community
+// Date: June 22, 2016
+// Version: 1
+// Available: https://stackoverflow.com/questions/37968011/how-to-call-finishaffinity-in-service-class
+
+// Title: findViewById in Kotlin - Android Ideas
+// Author: Medium Article
+// Date: [Accessed May 26, 2025]
+// Version: 1
+// Available: https://medium.com/android-ideas/findviewbyid-in-kotlin-ce4d22193c79
+
+// Title: Build a responsive UI with ConstraintLayout
+// Author: Android Developers
+// Date: [Accessed May 26, 2025]
+// Version: 1
+// Available: https://developer.android.com/develop/ui/views/layout/constraint-layout
+
+// Title: Kotlin When Expressions
+// Author: W3Schools
+// Date: [Accessed May 26, 2025]
+// Version: 1
+// Available: https://www.w3schools.com/kotlin/kotlin_when.php
+
+
